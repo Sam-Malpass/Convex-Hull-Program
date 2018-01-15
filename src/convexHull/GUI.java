ab5cc1532b47257ceb5d1f2e2861bf0437c23db9
Sam <sam@malpass.me.uk>
2018-01-15 09:29:48 +0000

    Add variables to GUI

--- src/convexHull/GUI.java
+++ src/convexHull/GUI.java
@@ -1,8 +1,16 @@
 package convexHull;
 
 import javafx.application.Application;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 public class GUI extends Application
 {
-
+	private int cSize = 512;
+	public int hullNo;
+	private GraphicsContext gContext; 
+	private VBox rPane;
+	private HBox lPane;
+	private Coordinate[] c1, c2, hull1, hull2;
 }

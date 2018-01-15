ed01818e4ebb6350d35ea2efbd9c159c3a64e0df
Sam <sam@malpass.me.uk>
2018-01-15 09:14:56 +0000

    Add cross function to Algorithm

--- src/convexHull/Algorithm.java
+++ src/convexHull/Algorithm.java
@@ -2,5 +2,8 @@ package convexHull;
 
 public class Algorithm 
 {
-
+	public static long cross(Coordinate o, Coordinate a, Coordinate b) 
+	{
+		return (a.getXPosition() - o.getXPosition()) * (b.getYPosition() - o.getYPosition()) - (a.getYPosition() - o.getYPosition()) * (b.getXPosition() - o.getXPosition());
+	}
 }

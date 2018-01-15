fc74ec15c448d7f136d2088e8faa4df93b917a90
Sam <sam@malpass.me.uk>
2018-01-15 09:07:52 +0000

    Add compareTo function to Coordinate

--- src/convexHull/Coordinate.java
+++ src/convexHull/Coordinate.java
@@ -4,4 +4,18 @@ public class Coordinate implements Comparable<Coordinate>
 {
 	public int xPosition;
 	public int yPosition;
+	
+	public int compareTo(Coordinate c) 
+	{
+		if (this.xPosition == c.xPosition) 
+		{
+			return this.yPosition - c.yPosition;
+		} 
+		else 
+		{
+			return this.xPosition - c.xPosition;
+		}
+	}
+	
+	
 }

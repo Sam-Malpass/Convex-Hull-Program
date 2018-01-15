b2800363b3bdf0ad9dfdc336d4c998604f296b29
Sam <sam@malpass.me.uk>
2018-01-15 09:27:27 +0000

    Add load function to FileIO

--- src/convexHull/FileIO.java
+++ src/convexHull/FileIO.java
@@ -1,38 +1,42 @@
 package convexHull;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.StringTokenizer;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 
 public class FileIO 
 {
-	public boolean save(String S)
+	public boolean save(String s)
 	{
-		JFrame Frame = new JFrame();
-		JFileChooser SaveAs = new JFileChooser();
-		SaveAs.setDialogTitle("Save As");
-		int Option = SaveAs.showSaveDialog(Frame);
+		JFrame frame = new JFrame();
+		JFileChooser saveAs = new JFileChooser();
+		saveAs.setDialogTitle("Save As");
+		int Option = saveAs.showSaveDialog(frame);
 		if (Option == JFileChooser.APPROVE_OPTION) 
 		{
-			File File = SaveAs.getSelectedFile();
-			String FilePath = File.getPath();
-			if(!FilePath.toLowerCase().endsWith(".in"))
+			File file = saveAs.getSelectedFile();
+			String filePath = file.getPath();
+			if(!filePath.toLowerCase().endsWith(".in"))
 			{
-			    File = new File(FilePath + ".in");
+			    file = new File(filePath + ".in");
 			}
 			try 
 			{	
-				PrintWriter Output = new PrintWriter(File);
-				Output.println(S);
-				Output.close();
+				PrintWriter output = new PrintWriter(file);
+				output.println(s);
+				output.close();
 				return true;
 			} 
-			catch (FileNotFoundException Exception) 
+			catch (FileNotFoundException e) 
 			{
-				Exception.printStackTrace();
+				e.printStackTrace();
 				return false;
 			}
 		}
@@ -40,5 +44,39 @@ public class FileIO
 		{
 			return false;
 		}
+	}
+	public Coordinate[] load()
+	{
+		Coordinate c[] = null;
+		JFrame frame = new JFrame();
+		JFileChooser loadFrom = new JFileChooser();
+		loadFrom.setDialogTitle("Load From");
+		int option = loadFrom.showOpenDialog(frame);
+		if(option == JFileChooser.APPROVE_OPTION)
+		{
+			File file = loadFrom.getSelectedFile();
+			try (FileReader fileReader = new FileReader(file))
+			{
+				BufferedReader bufferedFileReader = new BufferedReader(fileReader);
+				StringTokenizer stringTokenizer = new StringTokenizer(bufferedFileReader.readLine());
+				c = new Coordinate[Integer.parseInt(stringTokenizer.nextToken())];
+				for(int I = 0; I < c.length; I++)
+				{
+					c[I] = new Coordinate();
+					c[I].setXPosition(Integer.parseInt(stringTokenizer.nextToken()));
+					c[I].setYPosition(Integer.parseInt(stringTokenizer.nextToken()));
+				}
+	            bufferedFileReader.close();
+			} 
+			catch (FileNotFoundException e) 
+			{
+				e.printStackTrace();
+			} 
+			catch (IOException e) 
+			{
+				e.printStackTrace();
+			}
+		}
+		return c;
 	}
 }

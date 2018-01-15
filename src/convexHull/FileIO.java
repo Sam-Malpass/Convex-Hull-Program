34c27e34db7784f5d792bd973be9f5b38ce9cd9d
Sam <sam@malpass.me.uk>
2018-01-15 09:22:06 +0000

    Add save function to FileIO

--- src/convexHull/FileIO.java
+++ src/convexHull/FileIO.java
@@ -1,5 +1,44 @@
 package convexHull;
 
-public class FileIO {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+
+public class FileIO 
+{
+	public boolean save(String S)
+	{
+		JFrame Frame = new JFrame();
+		JFileChooser SaveAs = new JFileChooser();
+		SaveAs.setDialogTitle("Save As");
+		int Option = SaveAs.showSaveDialog(Frame);
+		if (Option == JFileChooser.APPROVE_OPTION) 
+		{
+			File File = SaveAs.getSelectedFile();
+			String FilePath = File.getPath();
+			if(!FilePath.toLowerCase().endsWith(".in"))
+			{
+			    File = new File(FilePath + ".in");
+			}
+			try 
+			{	
+				PrintWriter Output = new PrintWriter(File);
+				Output.println(S);
+				Output.close();
+				return true;
+			} 
+			catch (FileNotFoundException Exception) 
+			{
+				Exception.printStackTrace();
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
 }
